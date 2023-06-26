0261636c072ef92fd3959f5ba56c403135dab1f6
Анастасия Попкова <131842962+buykoam@users.noreply.github.com>
2023-06-26 19:17:57 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,12 +4,6 @@ public class Calculator {
     public int sum(int a, int b) {
         return a + b;
     }
-    public int div(int a, int b) {
-        return a / b;
-    }
-    public double div(double a, double b) {
-        return a / b;
-    }
     public String getLevel() {
         return level;
     }
