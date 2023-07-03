d7526cadff1ee44458981986305d955bc19a9420
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-07-03 09:24:06 +0300

    fixed 03/07/2023

--- src/test/java/CalcTestHW.java
+++ src/test/java/CalcTestHW.java
@@ -2,10 +2,19 @@ import factory.BrowserFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
 
 public class CalcTestHW {
     protected WebDriver driver;
@@ -14,6 +23,8 @@ public class CalcTestHW {
     public void setUp() {
         BrowserFactory browserFactory = new BrowserFactory();
         driver = browserFactory.getDriver();
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
     }
 
     @Test
@@ -21,6 +32,10 @@ public class CalcTestHW {
         driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
+
+
+        List<WebElement> buttonsList = driver.findElements(By.tagName("button"));
+        System.out.println(buttonsList.size());
 
         WebElement widthInput = driver.findElement(By.id("el_f_width"));
         WebElement lengthInput = driver.findElement(By.id("el_f_lenght"));
@@ -33,17 +48,19 @@ public class CalcTestHW {
 
         Select selectType = new Select(selectWebElement);
         selectType.selectByIndex(3);
-        Thread.sleep(2000);
-
         Select selectHeating = new Select(selectWebElement2);
         selectHeating.selectByIndex(2);
-        Thread.sleep(2000);
 
         calcButton.click();
 
-        Assert.assertEquals(driver.findElement(By.id("floor_cable_power")).getText(),
-                "28620");
-        Assert.assertEquals(driver.findElement(By.id("spec_floor_cable_power")).getText(),
-                "95");
+        assertTrue(driver.findElement(By.id("floor_cable_power")).getAttribute("value").equals("28620"),
+                "Не верно высчитана Мощь нагревательного кабеля или мата, Вт");
+        assertTrue(driver.findElement(By.id("spec_floor_cable_power")).getAttribute("value").equals("95"),
+                "Не верно высчитана Удельная мощность нагревательного кабеля или мата, Вт/м2");
+                }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
+
