fef76cacc3e6405e6545950bcfedd9357c2d791e
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-08-27 10:08:58 +0300

    Add files 27/08/23

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -4,8 +4,9 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
-        glue = "stepDefs"
+        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty.html"},
+        glue = "stepDefs",
+        tags = "@gui"
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
